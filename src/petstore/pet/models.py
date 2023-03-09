e27f9ca448d10db4f76646691b5095cce5bce359
Erhan <erhan.citil@maykinmedia.nl>
2023-03-09 11:16:00 +0100

    🗃️  [#51] Created models for the project

--- src/petstore/pet/models.py
+++ src/petstore/pet/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Pet(models.Model):
+    owner = models.ForeignKey('User', on_delete=models.CASCADE, related_name='pet')
+    breed = models.ForeignKey('Breed', on_delete=models.CASCADE, related_name='pet')
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=100)
+    skin_color = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
+
+class Image(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE, related_name='image')
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.id
+    
+class Breed(models.Model):
+    name = models.CharField(max_length=100)
+    origin = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
